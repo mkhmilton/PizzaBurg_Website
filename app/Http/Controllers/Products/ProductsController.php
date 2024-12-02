6f964b3e5efac52cad99e804815e919a1228e17d
mkhmilton <mkhmilton786@gmail.com>
2024-12-02 10:36:36 +0600

    project files

---
+++ app/Http/Controllers/Products/ProductsController.php
@@ -0,0 +1,199 @@
+<?php
+
+namespace App\Http\Controllers\Products;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Product\Product;
+use Auth;
+use App\Models\Product\Cart;
+use Redirect;
+use Session;
+use App\Models\Product\Order;
+use App\Models\Product\Booking;
+
+
+
+class ProductsController extends Controller
+{
+
+
+    public function singleProduct($id) {
+
+        $product = Product::find($id);
+
+
+        $relatedProducts = Product::where('type', $product->type)
+         ->where('id', '!=', $id)->take('4')
+         ->orderBy('id', 'desc')
+         ->get();
+
+        if(isset(Auth::user()->id)) {
+           
+    
+            //checking for products in cart
+            
+            $checkingInCart = Cart::where('pro_id', $id)
+             ->where('user_id', Auth::user()->id)
+             ->count();
+
+             return view('products.productsingle', compact('product', 'relatedProducts', 'checkingInCart'));
+
+        } else {
+            return view('products.productsingle', compact('product', 'relatedProducts'));
+
+        }
+        
+
+    }
+
+    public function addCart(Request $request, $id) {
+
+       $addCart = Cart::create([
+        "pro_id" => $request->pro_id,
+        "name" => $request->name,
+        "image" => $request->image,
+        "price" => $request->price,
+        "user_id" => Auth::user()->id,
+       ]);
+
+       return Redirect::route('product.single', $id)->with( ['success' => "product added to cart succesffully"] );
+    }
+
+
+    public function cart() {
+
+        $cartProducts = Cart::where('user_id', Auth::user()->id)
+        ->orderBy('id', 'desc')
+        ->get();
+
+
+        $totalPrice = Cart::where('user_id', Auth::user()->id)
+         ->sum('price');
+         
+        return view('products.cart', compact('cartProducts', 'totalPrice'));
+        
+    }
+
+
+    public function deleteProductCart($id) {
+
+        $deleteProducCart = Cart::where('pro_id', $id)
+         ->where('user_id', Auth::user()->id);
+         
+         
+        $deleteProducCart->delete();
+
+
+        if($deleteProducCart) {
+            return Redirect::route('cart')->with( ['delete' => "product deleted from cart succesffully"] );
+
+        } 
+        
+    }
+
+    
+    public function prepareCheckout(Request $request) {
+
+      $value = $request->price;
+
+      $price = Session::put('price', $value);
+
+      $newPrice = Session::get($price);
+
+        if($newPrice > 0) {
+            return Redirect::route('checkout');
+
+        } 
+        
+    }
+    
+
+    public function checkout() {
+
+
+       
+            return view('products.checkout');
+
+        
+        
+    }
+    
+    public function storeCheckout(Request $request) {
+
+        $checkout = Order::create($request->all());
+
+        if($checkout) {
+            return Redirect::route('products.pay');
+
+        }
+    }
+
+    public function payWithPaypal()  {
+
+
+ 
+        return view('products.pay');
+    }
+
+    public function success()  {
+
+        $deleteItems = Cart::where('user_id', Auth::user()->id);
+        $deleteItems->delete();
+
+
+        if($deleteItems) {
+
+            Session::forget('price');
+
+            return view('products.success');
+
+        }
+ 
+    }
+
+
+    public function BookTables(Request $request) {
+
+
+        Request()->validate([
+            "first_name" => "required|max:40",
+            "last_name" => "required|max:40",
+            "date" => "required",
+            "time" => "required",
+            "phone" => "required|max:40",
+            "message" => "required",
+        ]);
+
+        if ($request->date > date('n/j/Y')) {
+            $bookTables = Booking::create($request->all());
+
+            if($bookTables) {
+                return Redirect::route('home')->with( ['booking' => "you booked a table successfully"] );
+    
+            }
+        } else {
+            return Redirect::route('home')->with( ['date' => "invalide date, choose a date in the future"] );
+
+
+        }
+
+        
+    }
+    
+    public function menu()  {
+
+       $desserts = Product::select()->where("type", "desserts")->orderBy('id', 'desc')
+       ->take(4)->get();
+
+       $drinks = Product::select()->where("type", "drinks")->orderBy('id', 'desc')
+       ->take(4)->get();
+       
+
+       return view('products.menu', compact('desserts', 'drinks'));
+
+        
+    }
+    
+    
+}
